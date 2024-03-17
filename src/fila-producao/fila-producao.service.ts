fc0c7403ba44c2aaeb38b7abfdc3273880b51853
Rafael de Miranda <37124918+rafaeldemiranda95@users.noreply.github.com>
2024-03-17 19:50:32 -0300

    Adicionando o pubsub

--- src/fila-producao/fila-producao.service.ts
+++ src/fila-producao/fila-producao.service.ts
@@ -1,48 +1,71 @@
-/* eslint-disable prettier/prettier */
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { FilaProducao } from './fila-producao.entity/fila-producao.entity';
 import { Repository } from 'typeorm';
+import { PubSub } from '@google-cloud/pubsub';
+import { FilaProducao } from './fila-producao.entity/fila-producao.entity';
 import { CreatePedidoDto } from 'src/pedido/pedido.dto';
-import {
-  CreateFilaProducaoDto,
-  EditeFilaProducaoDto,
-} from './fila-producao.dto';
-import { PedidoService } from 'src/pedido/pedido.service';
 
 @Injectable()
-export class FilaProducaoService {
+export class FilaProducaoService implements OnModuleInit {
+  private pubSubClient = new PubSub();
+  private topicName = process.env.TOPIC_STATUS_ATUALIZADO || 'topic-status-atualizado';
+  private subscriptionName = process.env.SUBSCRIPTION_PEDIDO_REALIZADO || 'subscription-pedido-realizado';
+
   constructor(
     @InjectRepository(FilaProducao)
     private filaProducaoRepository: Repository<FilaProducao>,
-    private readonly pedidoService: PedidoService,
-  ) {}
+  ) { }
 
-  async findAll(): Promise<FilaProducao[]> {
-    return this.filaProducaoRepository.find({
-      relations: ['pedido'],
+  async onModuleInit() {
+    this.listenForPedidoRealizadoMessages();
+  }
+
+  private listenForPedidoRealizadoMessages() {
+    const subscription = this.pubSubClient.subscription(this.subscriptionName);
+
+    subscription.on('message', async message => {
+      console.log('Recebida mensagem:', message.data.toString());
+      const pedidoData: CreatePedidoDto = JSON.parse(message.data.toString());
+
+      await this.addPedidoToFila(pedidoData);
+      message.ack();
+    });
+
+    subscription.on('error', error => {
+      console.error('Erro ao receber mensagem:', error);
     });
   }
-  async addPedidoToFila(
-    createPedidoDto: CreatePedidoDto,
-  ): Promise<FilaProducao> {
-    const pedido = await this.pedidoService.salvarPedido(createPedidoDto);
 
-    const filaProducao: CreateFilaProducaoDto = {
+  async addPedidoToFila(createPedidoDto: CreatePedidoDto): Promise<FilaProducao> {
+    const filaProducao = this.filaProducaoRepository.create({
       status: 'pendente',
-      pedidoId: pedido.id,
-    };
+      pedidoId: createPedidoDto.id,
+    });
 
-    const fila = this.filaProducaoRepository.create(filaProducao);
+    const savedFilaProducao = await this.filaProducaoRepository.save(filaProducao);
 
-    return this.filaProducaoRepository.save(fila);
+    await this.processarPedidoNaFila(savedFilaProducao);
+
+    return savedFilaProducao;
   }
 
-  async editeStatusFilaProducao(
-    id: number,
-    editeFilaProducaoDto: EditeFilaProducaoDto,
-  ): Promise<FilaProducao> {
-    await this.filaProducaoRepository.update(id, editeFilaProducaoDto);
-    return this.filaProducaoRepository.findOneBy({ id });
+  private async processarPedidoNaFila(filaProducao: FilaProducao) {
+    filaProducao.status = 'completo';
+    await this.filaProducaoRepository.save(filaProducao);
+    await this.publicarStatusPedidoAtualizado(filaProducao);
+  }
+
+  private async publicarStatusPedidoAtualizado(filaProducao: FilaProducao) {
+    const message = {
+      pedidoId: filaProducao.pedidoId,
+      status: filaProducao.status,
+    };
+    const dataBuffer = Buffer.from(JSON.stringify(message));
+    try {
+      await this.pubSubClient.topic(this.topicName).publish(dataBuffer);
+      console.log(`Mensagem publicada no tópico ${this.topicName}`);
+    } catch (error) {
+      console.error(`Erro ao publicar mensagem: ${error.message}`);
+    }
   }
 }
